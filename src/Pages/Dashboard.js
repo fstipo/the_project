58d20ef6e83d7c3ec8b2835e46c6caedb9034648
fstipo <fstipanov@gmail.com>
2022-04-08 16:21:28 +0200

    add a Api

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,39 +1,166 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import MainTitle from '../Components/Main/MainTitle';
 import { AgGridReact } from 'ag-grid-react';
+import '../Components/Layout/Container/Container.css';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const Project = () => {
   const [columnDefs] = useState([
-    { field: 'first' },
-    { field: 'last' },
-    { field: 'full' },
-    { field: 'age' },
+    {
+      field: 'first',
+      checkboxSelection: true,
+      sortable: 'true',
+      filter: 'true',
+      headerTooltip: 'First Name',
+      resizable: true,
+      width: 150,
+    },
+    {
+      field: 'last',
+      sortable: 'true',
+      filter: 'true',
+      headerTooltip: 'Last Name',
+      resizable: true,
+      width: 120,
+    },
+    {
+      field: 'email',
+      sortable: 'true',
+      filter: 'true',
+      headerTooltip: 'Email',
+      width: 220,
+    },
+    {
+      field: 'address',
+      sortable: 'true',
+      filter: 'true',
+      headerTooltip: 'Full Name',
+      width: 230,
+    },
+    {
+      field: 'created',
+      sortable: 'true',
+      filter: 'true',
+      headerTooltip: 'Age',
+      width: 165,
+    },
+    { field: 'balance', sortable: 'true', filter: 'true' },
   ]);
 
-  const [rowData] = useState([
-    { first: 'John', last: 'Grayner', full: 'John Grayner', age: 39 },
-    { first: 'Franko', last: 'Stipanov', full: 'Franko Stipanov', age: 44 },
-    { first: 'Goran', last: 'Jakovljević', full: 'Goran Jakovljević', age: 43 },
-    { first: 'Mary', last: 'Black', full: 'Mary Black', age: 14 },
-    { first: 'Josipa', last: 'Stipanov', full: 'Josipa Stipanov', age: 43 },
-    { first: 'Mike', last: 'Jordan', full: 'Mike Jordan', age: 55 },
-    { first: 'Johny', last: 'Lennon', full: 'Johny Lennon', age: 33 },
-    { first: 'Marty', last: 'Moon', full: 'Marty Moon', age: 65 },
-    { first: 'Victory', last: 'Hope', full: 'Victory Hope', age: 23 },
-    { first: 'Robert', last: 'Hill', full: 'Robert Hill', age: 98 },
-    { first: 'Alan', last: 'Ford', full: 'Alan Ford', age: 19 },
-    { first: 'Sue', last: 'Ford', full: 'Sue Ford', age: 26 },
-  ]);
+  const [rowData, setRowData] = useState([]);
+
+  useEffect(() => {
+    // fetch('https://www.ag-grid.com/example-assets/row-data.json')
+    // fetch('https://jsonplaceholder.typicode.com/users')
+
+    fetch(
+      'https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole'
+    )
+      .then((response) => response.json())
+      .then((data) => setRowData(data));
+  }, []);
+
+  // const [rowData] = useState([
+  //   {
+  //     first: 'John',
+  //     last: 'Grayner',
+  //     full: 'John Grayner',
+  //     age: 39,
+  //     email: 'john@gmail.com',
+  //   },
+  //   {
+  //     first: 'Franko',
+  //     last: 'Stipanov',
+  //     full: 'Franko Stipanov',
+  //     age: 44,
+  //     email: 'franko@gmail.com',
+  //   },
+  //   {
+  //     first: 'Goran',
+  //     last: 'Jakovljević',
+  //     full: 'Goran Jakovljević',
+  //     age: 43,
+  //     email: 'goran@gmail.com',
+  //   },
+  //   {
+  //     first: 'Mary',
+  //     last: 'Black',
+  //     full: 'Mary Black',
+  //     age: 14,
+  //     email: 'mary@gmail.com',
+  //   },
+  //   {
+  //     first: 'Josipa',
+  //     last: 'Stipanov',
+  //     full: 'Josipa Stipanov',
+  //     age: 43,
+  //     email: 'josipa@gmail.com',
+  //   },
+  //   {
+  //     first: 'Mike',
+  //     last: 'Jordan',
+  //     full: 'Mike Jordan',
+  //     age: 55,
+  //     email: 'mike@gmail.com',
+  //   },
+  //   {
+  //     first: 'Johny',
+  //     last: 'Lennon',
+  //     full: 'Johny Lennon',
+  //     age: 33,
+  //     email: 'johnny@gmail.com',
+  //   },
+  //   { first: 'Marty', last: 'Moon', full: 'Marty Moon', age: 65 },
+  //   {
+  //     first: 'Victory',
+  //     last: 'Hope',
+  //     full: 'Victory Hope',
+  //     age: 23,
+  //     email: 'victory@gmail.com',
+  //   },
+  //   { first: 'Robert', last: 'Hill', full: 'Robert Hill', age: 98 },
+  //   {
+  //     first: 'Alan',
+  //     last: 'Ford',
+  //     full: 'Alan Ford',
+  //     age: 19,
+  //     email: 'alan@gmail.com',
+  //   },
+  //   {
+  //     first: 'Sue',
+  //     last: 'Ford',
+  //     full: 'Sue Ford',
+  //     age: 26,
+  //     email: 'sue@gmail.com',
+  //   },
+  // ]);
+
+  const gridOptions = {
+    pagination: true,
+    paginationAutoPageSize: true,
+  };
+
+  const onGridReady = (e) => {
+    e.api.sizeColumnsToFit();
+    e.columnApi.resetColumnState();
+  };
 
   return (
     <>
       <MainTitle name="Dashboard" icon="speedometer2" />
-      <div className="container-xl mx-5">
-        <div className="ag-theme-alpine" style={{ height: 800, width: 800 }}>
-          <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+      <div className="container-fluid d-flex justify-content-center">
+        <div
+          className="table ag-theme-alpine"
+          style={{ width: '100%', height: '100%;' }}
+        >
+          <AgGridReact
+            rowData={rowData}
+            columnDefs={columnDefs}
+            gridOptions={gridOptions}
+            onGridReady={onGridReady}
+          ></AgGridReact>
         </div>
       </div>
     </>
