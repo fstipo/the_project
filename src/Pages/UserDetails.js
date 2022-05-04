5143d3c4f98a8dae492f0d5c97fe70c946002612
fstipo <fstipanov@gmail.com>
2022-05-04 15:46:47 +0200

    add user form

--- src/Pages/UserDetails.js
+++ src/Pages/UserDetails.js
@@ -13,21 +13,116 @@ const UserDetails = (props) => {
     )
       .then((response) => response.json())
       .then((data) => {
-        let newData = data
-          .map((item) => (item.id = item.email))
-          .find((item) => item.id === userId);
-        console.log(newData);
-        console.log(userId);
-        setUserData(data);
+        let newData = data.map((item) => (item.id = item.email));
+        // .findIndex((n) => n === userId);
+
+        console.log(data[0]);
+        setUserData(data[0]);
       });
-  }, []);
+  }, [userId]);
 
-  const findUser = () => {
-    console.log('page is loaded');
-    console.log(userData);
-  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <div className="my-5">
+            <h3>
+              User Details: {userData.first} {userData.last}
+            </h3>
+            <hr />
+          </div>
 
-  return <h1 onLoad={findUser}>Hello my old friend! Your id is {userId}</h1>;
+          <form className="file-upload">
+            <div className="row mb-5 gx-5">
+              <div className="col-xxl-8 mb-5 mb-xxl-0">
+                <div className="bg-secondary-soft px-4 py-5 rounded">
+                  <div className="row g-3">
+                    <h4 className="mb-4 mt-0">Contact detail</h4>
+
+                    <div className="col-md-6">
+                      <label className="form-label">First Name *</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder=""
+                        aria-label="First name"
+                        value={userData.first}
+                      />
+                    </div>
+
+                    <div className="col-md-6">
+                      <label className="form-label">Last Name *</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder=""
+                        aria-label="Last name"
+                        value={userData.last}
+                      />
+                    </div>
+
+                    <div className="col-md-6">
+                      <label htmlFor="inputEmail4" className="form-label">
+                        Email *
+                      </label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="inputEmail4"
+                        value={userData.email}
+                      />
+                    </div>
+
+                    <div className="col-md-6">
+                      <label className="form-label">Address *</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder=""
+                        aria-label="Address"
+                        value={userData.address}
+                      />
+                    </div>
+
+                    <div className="col-md-6">
+                      <label className="form-label">Created *</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder=""
+                        aria-label="created"
+                        value={userData.created}
+                      />
+                    </div>
+
+                    <div className="col-md-6">
+                      <label className="form-label">Balance *</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder=""
+                        aria-label="Phone number"
+                        value={userData.balance}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="gap-3 d-md-flex justify-content-center text-center">
+                <button type="button" className="btn btn-danger btn-lg">
+                  Delete profile
+                </button>
+                <button type="button" className="btn btn-primary btn-lg">
+                  Update profile
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserDetails;
